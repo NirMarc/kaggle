0cb825596e800ba3d941fd1ae397abab754d2dac
Nir Marciano <marciano.nir@gmail.com>
2025-11-01 17:22:14 +0200

    Created a script for downloading data

---
+++ spaceship-titanic/donwload-data.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+echo "Downloading Spaceship Titanic dataset from Kaggle..."
+mkdir -p ./data # Create data directory if it doesn't exist
+kaggle competitions download -c spaceship-titanic -p ./data # Download dataset -p specifies the path
+unzip ./data/spaceship-titanic.zip -d ./data # Unzip the downloaded file into the data directory -d specifies the destination destination
+rm ./data/spaceship-titanic.zip
+echo "Download complete. Files are saved in the ./data directory."
+ls ./data
---
+++ spaceship-titanic/learning.md
@@ -0,0 +1,2 @@
+### Download Data
+- In kaggle you can download data using bash, creating a script makes it look more professional and it takes a minute to do.
